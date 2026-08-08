6478e7bae5f947463148cc112fff392005f101ec
ashsidrah716 <ashsidrah7@gmail.com>
2026-08-08 15:13:11 +0500

    Implement task filtering

--- todo.js
+++ todo.js
@@ -2,15 +2,22 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteComplete);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event) {
   // prevent form from submitting
   event.preventDefault();
+
+  // prevent empty task
+  if (!todoInput.value.trim()) {
+    return;
+  }
 
   // todo div
   const todoDiv = document.createElement("div");
@@ -47,11 +54,40 @@ function deleteComplete(e) {
   // delete todo
   if (item.classList[0] === "delete-btn") {
     const todo = item.parentElement;
-    todo.remove();
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
   }
 
+  // mark todo as complete
   if (item.classList.contains("complete-btn")) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(event) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "complete":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "incomplete":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
